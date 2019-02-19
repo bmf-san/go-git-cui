e174af4823a920afa74bd4e869e5074ff527de2e
bmf-san <bmf.infomation@gmail.com>
2019-02-20 02:01:48 +0900

    [update]

--- main.go
+++ main.go
@@ -1,60 +1,47 @@
 package main
 
 import (
-	"bufio"
-	"flag"
 	"fmt"
-	"os"
 	"os/exec"
 )
 
-func main() {
-	var (
-		msg   = flag.String("m", "default message.", "Message")
-		num   = flag.Uint("n", 0, "Count(>=0)")
-		debug = flag.Bool("debug", false, "Debug Mode enabled?")
-	)
+func gitCheckoutToRemoteBranch() {
+	out, err := exec.Command("git", "branch", "-r").Output()
 
-	flag.Parse()
-
-	fmt.Printf("param -m -> %s\n", *msg) // as -m option
-	fmt.Printf("param -n -> %s\n", *num) // as -n option
-	fmt.Printf("param -debug -> %t\n", *debug)
-
-	if Question("今日は元気ですか？[y/n] ") {
-		out, err := exec.Command("ls").Output()
-
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Printf("ls result: \n%s", string(out))
-	} else {
-		fmt.Println("元気出してくださいね！")
-	}
-}
-
-func Question(q string) bool {
-	result := true
-	fmt.Print(q)
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		i := scanner.Text()
-
-		if i == "Y" || i == "y" {
-			break
-		} else if i == "N" || i == "n" {
-			result = false
-			break
-		} else {
-			fmt.Println("yかnで答えてください。")
-			fmt.Print(q)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
+	if err != nil {
 		panic(err)
 	}
-	return result
+
+	fmt.Println("%T", out)
+
+	// scanner := bufio.NewScanner(os.Stdin)
+	// for scanner.Scan() {
+	// 	i := scanner.Text()
+
+	// 	if i == "Y" || i == "y" {
+	// 		break
+	// 	} else if i == "N" || i == "n" {
+	// 		result = false
+	// 		break
+
+	// 		fmt.Println("yかnで答えてください。")
+	// 		fmt.Print(q)
+	// 	}
+	// }
+
+	// if err := scanner.Err(); err != nil {
+	// 	panic(err)
+	// }
+
+	// if result {
+
+	// } else {
+
+	// }
+
+	// fmt.Println(string(out))
+}
+
+func main() {
+	gitCheckoutToRemoteBranch()
 }
