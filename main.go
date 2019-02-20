e1d0e8590d831a534a3ead60a453f9ee97b93917
bmf-san <bmf.infomation@gmail.com>
2019-02-21 02:39:15 +0900

    [update]

--- main.go
+++ main.go
@@ -3,60 +3,32 @@ package main
 import (
 	"fmt"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
-func gitCheckoutToRemoteBranch() {
+// 最終的にバイナリ配布を想定して、コマンド関数はオプション（エイリアス名）またはサブコマンド（ex. go-git g-c-r-b)
+func showRemoteBranch() {
 	out, err := exec.Command("git", "branch", "-r").Output()
 
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(string(out))
+	var branches []string
 
-	// 文字列を1行入力
-	// func StrStdin() (stringInput string) {
-	// 	scanner := bufio.NewScanner(os.Stdin)
+	for _, v := range regexp.MustCompile("\r\n|\n\r|\n|\r").Split(string(out), -1) {
+		if v != "" {
+			branches = append(branches, strings.TrimSpace(string(v)))
+		}
+	}
 
-	// 	scanner.Scan()
-	// 	stringInput = scanner.Text()
-
-	// 	stringInput = strings.TrimSpace(stringInput)
-	// 	return
-	// }
-
-	// func main() {
-	// 	p := StrStdin()
-	// 	fmt.Println(p)
-	// }
-	// scanner := bufio.NewScanner(os.Stdin)
-	// for scanner.Scan() {
-	// 	i := scanner.Text()
-
-	// 	if i == "Y" || i == "y" {
-	// 		break
-	// 	} else if i == "N" || i == "n" {
-	// 		result = false
-	// 		break
-
-	// 		fmt.Println("yかnで答えてください。")
-	// 		fmt.Print(q)
-	// 	}
-	// }
-
-	// if err := scanner.Err(); err != nil {
-	// 	panic(err)
-	// }
-
-	// if result {
-
-	// } else {
-
-	// }
-
-	// fmt.Println(string(out))
+	fmt.Println("Remote Branches:")
+	for i, v := range branches {
+		fmt.Println(i, v)
+	}
 }
 
 func main() {
-	gitCheckoutToRemoteBranch()
+	showRemoteBranch()
 }
