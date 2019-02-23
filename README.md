94a16554fa69c6a26773c3aba00baaeaa381a066
bmf-san <bmf.infomation@gmail.com>
2019-02-24 01:16:00 +0900

    [update]

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # go-git-cui
 This is a tool for git command built with Golang.
 
-# Note
-clone
-add
-commit
-push
-rebase
-pull
-fetch
-
 # References
 - [Golang: Implementing subcommands for command line applications](http://blog.ralch.com/tutorial/golang-subcommands/)
 - [Golangで実装するCUIコマンド](https://qiita.com/curious-eyes/items/5c109043d0a0c01bb54a)
--- main.go
+++ main.go
@@ -6,18 +6,16 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-// 最終的にバイナリ配布を想定して、コマンド関数はオプション（エイリアス名）またはサブコマンド（ex. go-git g-c-r-b)
-func showRemoteBranch() {
+func getRemoteBranches() (branches []string) {
 	out, err := exec.Command("git", "branch", "-r").Output()
 
 	if err != nil {
 		panic(err)
 	}
-
-	var branches []string
 
 	for _, v := range regexp.MustCompile("\r\n|\n\r|\n|\r").Split(string(out), -1) {
 		if v != "" {
@@ -25,35 +23,28 @@ func showRemoteBranch() {
 		}
 	}
 
+	return branches
+}
+
+func checkOutToRemoteBranch(branches []string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input, _ := strconv.Atoi(scanner.Text())
+
+	_, err := exec.Command("git", "checkout", "-b", branches[input], branches[input]).Output()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	branches := getRemoteBranches()
 	fmt.Println("Remote Branches:")
 	for i, v := range branches {
 		fmt.Println(i, v)
 	}
 
 	fmt.Println("Select a branch number:")
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		i := scanner.Text()
-
-		if i == "Y" || i == "y" {
-			break
-		} else if i == "N" || i == "n" {
-			result = false
-			break
-		} else {
-			fmt.Println("yかnで答えてください。")
-			fmt.Print(q)
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
-
-	resultをゴニョゴニョ
-}
-
-func main() {
-	showRemoteBranch()
+	checkOutToRemoteBranch(branches)
 }
